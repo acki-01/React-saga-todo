c0d90287c63aa4b08858ef5b95999cbc2f7e50f5
arkadiuszizycki <zamf124@gmail.com>
2018-11-25 07:16:05 +0100

    extracting callbacks for reducers

--- src/store/reducers/todo.js
+++ src/store/reducers/todo.js
@@ -4,29 +4,40 @@ const initialState = {
     todos: []
 };
 
+const addToDo = (state, action) => ({
+    ...state,
+    todos: [...state.todos, action.newToDo]
+});
+
+const markAsDone = (state, action) => {
+    const updatedToDo = state.todos.map(todo => {
+        if (todo.key === action.toDoKey) {
+            todo.done = !todo.done;
+        }
+        return todo;
+    });
+    return {
+        ...state,
+        todos: updatedToDo
+    };
+};
+
+const removeToDo = (state, action) => {
+    const updatedToDo = state.todos.filter(todo => todo.key !== action.toDoKey);
+    return {
+        ...state,
+        todos: updatedToDo
+    };
+};
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.ADD_TODO:
-            console.log(action);
-            return {
-                ...state,
-                todos: [...state.todos, action.newToDo]
-            };
+            return addToDo(state, action);
         case actionTypes.MARK_AS_DONE_TODO:
-            return {
-                ...state,
-                todos: state.todos.map(todo => {
-                    if (todo.key === action.toDoKey) {
-                        todo.done = !todo.done;
-                    }
-                    return todo;
-                })
-            };
+            return markAsDone(state, action);
         case actionTypes.REMOVE_TODO:
-            return {
-                ...state,
-                todos: state.todos.filter(todo => todo.key !== action.toDoKey)
-            };
+            return removeToDo(state, action);
         default:
             return state;
     }
